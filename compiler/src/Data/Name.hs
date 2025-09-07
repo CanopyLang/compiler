0880fec2005ef8bff5e8a7d774d25233b8e8a714
Quinten Kasteel <quintenkasteel@gmail.com>
2025-09-07 15:47:26 +0200

    feat(data-structures): enhance core compiler data structures with modular organization
    - compiler/src/Data/Bag.hs
    - compiler/src/Data/Index.hs
    - compiler/src/Data/Map/Utils.hs
    - compiler/src/Data/Name.hs
    - compiler/src/Data/NonEmptyList.hs
    - compiler/src/Data/OneOrMore.hs
    - compiler/src/Data/Utf8.hs
    - compiler/src/Data/Name/Constants.hs
    - compiler/src/Data/Name/Core.hs
    - compiler/src/Data/Name/Generation.hs
    - compiler/src/Data/Name/Kernel.hs
    - compiler/src/Data/Name/TypeVariable.hs
    - compiler/src/Data/Utf8/Binary.hs
    - compiler/src/Data/Utf8/Builder.hs
    - compiler/src/Data/Utf8/Core.hs
    - compiler/src/Data/Utf8/Creation.hs
    - compiler/src/Data/Utf8/Encoding.hs
    - compiler/src/Data/Utf8/Manipulation.hs
    - compiler/src/Data/Utf8/Types.hs
    
    Files committed: 19

--- compiler/src/Data/Name.hs
+++ compiler/src/Data/Name.hs
@@ -1,618 +1,118 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE EmptyDataDecls#-}
-{-# LANGUAGE FlexibleInstances#-}
-{-# LANGUAGE MagicHash#-}
-{-# LANGUAGE UnboxedTuples #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
+-- | Name handling for the Canopy compiler.
+--
+-- This module provides a unified interface for all name operations in the
+-- Canopy compiler. It re-exports functionality from specialized sub-modules:
+--
+-- * "Data.Name.Core" - Basic name operations and type definitions
+-- * "Data.Name.Kernel" - Kernel name processing and type checking
+-- * "Data.Name.TypeVariable" - Type variable name generation
+-- * "Data.Name.Generation" - Complex name generation and combination
+-- * "Data.Name.Constants" - Predefined name constants
+--
+-- Names in Canopy are represented as UTF-8 encoded byte arrays for efficient
+-- memory usage and fast string operations. The Name type is used throughout
+-- the compiler for identifiers, module names, type names, and more.
+--
+-- === Usage Examples
+--
+-- @
+-- -- Basic name operations
+-- let name = fromChars "myFunction"
+--     chars = toChars name
+--
+-- -- Check for kernel operations
+-- when (isKernel name) $ do
+--   let kernelName = getKernel name
+--   processKernelOperation kernelName
+--
+-- -- Generate type variables
+-- let typeVar = fromTypeVariableScheme 0  -- "a"
+--     indexVar = fromVarIndex 1           -- "_v1"
+--
+-- -- Use predefined constants
+-- when (typeName == int) $
+--   applyIntTypeRules
+-- @
+--
+-- @since 0.19.1
 module Data.Name
-  ( Name
-  --
-  , toChars
-  , toCanopyString
-  , toBuilder
-  --
-  , fromPtr
-  , fromChars
-  --
-  , getKernel
-  , hasDot
-  , splitDots
-  , isKernel
-  , isNumberType
-  , isComparableType
-  , isAppendableType
-  , isCompappendType
-  , fromVarIndex
-  , fromWords
-  , fromManyNames
-  , fromTypeVariable
-  , fromTypeVariableScheme
-  , sepBy
-  --
-  , int, float, bool, char, string
-  , maybe, result, list, array, dict, tuple, jsArray
-  , task, router, cmd, sub, platform, virtualDom
-  , shader, debug, debugger, bitwise, basics
-  , utils, negate, true, false, value
-  , node, program, _main, _Main, dollar, identity
-  , replModule, replValueToPrint
+  ( Name,
+    CANOPY_NAME,
+
+    -- * Core Operations
+    toChars,
+    toCanopyString,
+    toBuilder,
+    fromPtr,
+    fromChars,
+    hasDot,
+    splitDots,
+
+    -- * Kernel Operations
+    getKernel,
+    isKernel,
+    isNumberType,
+    isComparableType,
+    isAppendableType,
+    isCompappendType,
+
+    -- * Type Variable Generation
+    fromVarIndex,
+    fromTypeVariable,
+    fromTypeVariableScheme,
+
+    -- * Name Generation
+    fromManyNames,
+    fromWords,
+    sepBy,
+
+    -- * Predefined Constants
+    int,
+    float,
+    bool,
+    char,
+    string,
+    maybe,
+    result,
+    list,
+    array,
+    dict,
+    tuple,
+    jsArray,
+    task,
+    router,
+    cmd,
+    sub,
+    platform,
+    virtualDom,
+    shader,
+    debug,
+    debugger,
+    bitwise,
+    basics,
+    utils,
+    negate,
+    true,
+    false,
+    value,
+    node,
+    program,
+    _main,
+    _Main,
+    dollar,
+    identity,
+    replModule,
+    replValueToPrint,
   )
-  where
-
-
-import Prelude hiding (length, maybe, negate)
-import Control.Exception (assert)
-import qualified Data.Binary as Binary
-import Data.ByteString.Builder (Builder)
-import qualified Data.Coerce as Coerce
-import qualified Data.List as List
-import qualified Data.String as Chars
-import qualified Data.Utf8 as Utf8
-import GHC.Exts
-  ( Int(I#)
-    , Ptr
-  , isTrue#
-  )
-import GHC.ST (ST(ST), runST)
-import GHC.Prim
-import GHC.Word (Word8(W8#))
-
-import qualified Canopy.String as ES
-
-
-
--- NAME
-
-
-type Name =
-  Utf8.Utf8 CANOPY_NAME
-
-
-data CANOPY_NAME
-
-
-
--- INSTANCES
-
-
-instance Chars.IsString (Utf8.Utf8 CANOPY_NAME) where
-  fromString = Utf8.fromChars
-
-instance Binary.Binary (Utf8.Utf8 CANOPY_NAME) where
-  get = Utf8.getUnder256
-  put = Utf8.putUnder256
-
-
-
--- TO
-
-
-toChars :: Name -> String
-toChars =
-  Utf8.toChars
-
-
-toCanopyString :: Name -> ES.String
-toCanopyString =
-  Coerce.coerce
-
-
-{-# INLINE toBuilder #-}
-toBuilder :: Name -> Builder
-toBuilder =
-  Utf8.toBuilder
-
-
-
--- FROM
-
-
-fromPtr :: Ptr Word8 -> Ptr Word8 -> Name
-fromPtr =
-  Utf8.fromPtr
-
-
-fromChars :: String -> Name
-fromChars =
-  Utf8.fromChars
-
-
-
--- HAS DOT
-
-
-hasDot :: Name -> Bool
-hasDot =
-  Utf8.contains 0x2E {- . -}
-
-
-splitDots :: Name -> [Name]
-splitDots =
-  Utf8.split 0x2E {- . -}
-
-
-
--- GET KERNEL
-
-
-getKernel :: Name -> Name
-getKernel name@(Utf8.Utf8 ba#) =
-  assert (isKernel name)
-  (
-    runST
-    (
-      let
-        -- "Canopy.Kernel." = 14 chars, "Elm.Kernel." = 11 chars
-        !prefixLen# = if Utf8.startsWith prefixKernel name then 14# else 11#
-        !size# = sizeofByteArray# ba# -# prefixLen#
-      in
-      ST $ \s ->
-        case newByteArray# size# s of
-          (# s, mba# #) ->
-            case copyByteArray# ba# prefixLen# mba# 0# size# s of
-              s ->
-                case unsafeFreezeByteArray# mba# s of
-                  (# s, ba# #) -> (# s, Utf8.Utf8 ba# #)
-    )
-  )
-
-
-
--- STARTS WITH
-
-
-isKernel :: Name -> Bool
-isKernel name = Utf8.startsWith prefixKernel name || Utf8.startsWith prefixElmKernel name
-
-isNumberType :: Name -> Bool
-isNumberType = Utf8.startsWith prefixNumber
-
-isComparableType :: Name -> Bool
-isComparableType = Utf8.startsWith prefixComparable
-
-isAppendableType :: Name -> Bool
-isAppendableType = Utf8.startsWith prefixAppendable
-
-isCompappendType :: Name -> Bool
-isCompappendType = Utf8.startsWith prefixCompappend
-
-{-# NOINLINE prefixKernel #-}
-prefixKernel :: Name
-prefixKernel = fromChars "Canopy.Kernel."
-
-{-# NOINLINE prefixElmKernel #-}
-prefixElmKernel :: Name
-prefixElmKernel = fromChars "Elm.Kernel."
-
-{-# NOINLINE prefixNumber #-}
-prefixNumber :: Name
-prefixNumber = fromChars "number"
-
-{-# NOINLINE prefixComparable #-}
-prefixComparable :: Name
-prefixComparable = fromChars "comparable"
-
-{-# NOINLINE prefixAppendable #-}
-prefixAppendable :: Name
-prefixAppendable = fromChars "appendable"
-
-{-# NOINLINE prefixCompappend #-}
-prefixCompappend :: Name
-prefixCompappend = fromChars "compappend"
-
-
-
--- FROM VAR INDEX
-
-
-fromVarIndex :: Int -> Name
-fromVarIndex n =
-  runST
-  (
-    do  let !size = 2 + getIndexSize n
-        mba <- newByteArray size
-        writeWord8 mba 0 0x5F {- _ -}
-        writeWord8 mba 1 0x76 {- v -}
-        writeDigitsAtEnd mba size n
-        freeze mba
-  )
-
-
-{-# INLINE getIndexSize #-}
-getIndexSize :: Int -> Int
-getIndexSize n
-  | n < 10  = 1
-  | n < 100 = 2
-  | otherwise    = ceiling (logBase 10 (fromIntegral n + 1) :: Float)
-
-
-
-writeDigitsAtEnd :: MBA s -> Int -> Int -> ST s ()
-writeDigitsAtEnd !mba !oldOffset !n =
-  do  let (q,r) = quotRem n 10
-      let !newOffset = oldOffset - 1
-      writeWord8 mba newOffset (0x30 + fromIntegral r)
-      if q <= 0
-        then return ()
-        else writeDigitsAtEnd mba newOffset q
-
-
-
--- FROM TYPE VARIABLE
-
-
-fromTypeVariable :: Name -> Int -> Name
-fromTypeVariable name@(Utf8.Utf8 ba#) index =
-  if index <= 0 then
-    name
-  else
-    let
-      len# = sizeofByteArray# ba#
-      end# = word8ToWord# (indexWord8Array# ba# (len# -# 1#))
-    in
-    if isTrue# (leWord# 0x30## end#) && isTrue# (leWord# end# 0x39##) then
-      runST
-      (
-        do  let !size = I# len# + 1 + getIndexSize index
-            mba <- newByteArray size
-            copyToMBA name mba
-            writeWord8 mba (I# len#) 0x5F {- _ -}
-            writeDigitsAtEnd mba size index
-            freeze mba
-      )
-    else
-      runST
-      (
-        do  let !size = I# len# + getIndexSize index
-            mba <- newByteArray size
-            copyToMBA name mba
-            writeDigitsAtEnd mba size index
-            freeze mba
-      )
-
-
-
--- FROM TYPE VARIABLE SCHEME
-
-
-fromTypeVariableScheme :: Int -> Name
-fromTypeVariableScheme scheme =
-  runST
-  (
-    if scheme < 26 then
-      do  mba <- newByteArray 1
-          writeWord8 mba 0 (0x61 + fromIntegral scheme)
-          freeze mba
-    else
-      do  let (extra, letter) = quotRem scheme 26
-          let !size = 1 + getIndexSize extra
-          mba <- newByteArray size
-          writeWord8 mba 0 (0x61 + fromIntegral letter)
-          writeDigitsAtEnd mba size extra
-          freeze mba
-  )
-
-
-
--- FROM MANY NAMES
---
--- Creating a unique name by combining all the subnames can create names
--- longer than 256 bytes relatively easily. So instead, the first given name
--- (e.g. foo) is prefixed chars that are valid in JS but not Canopy (e.g. _M$foo)
---
--- This should be a unique name since 0.19 disallows shadowing. It would not
--- be possible for multiple top-level cycles to include values with the same
--- name, so the important thing is to make the cycle name distinct from the
--- normal name. Same logic for destructuring patterns like (x,y)
-
-
-fromManyNames :: [Name] -> Name
-fromManyNames names =
-  case names of
-    [] ->
-      blank
-      -- NOTE: this case is needed for (let _ = Debug.log "x" x in ...)
-      -- but maybe unused patterns should be stripped out instead
-
-    Utf8.Utf8 ba# : _ ->
-      let
-        len# = sizeofByteArray# ba#
-      in
-      runST
-      (
-        ST $ \s ->
-          case newByteArray# (len# +# 3#) s of
-            (# s, mba# #) ->
-              case writeWord8Array# mba# 0# (wordToWord8# 0x5F##) {-_-} s of
-                s ->
-                  case writeWord8Array# mba# 1# (wordToWord8# 0x4D##) {-M-} s of
-                    s ->
-                      case writeWord8Array# mba# 2# (wordToWord8# 0x24##) {- $ -} s of
-                        s ->
-                          case copyByteArray# ba# 0# mba# 3# len# s of
-                            s ->
-                              case unsafeFreezeByteArray# mba# s of
-                                (# s, ba# #) -> (# s, Utf8.Utf8 ba# #)
-      )
-
-
-{-# NOINLINE blank #-}
-blank :: Name
-blank =
-  fromWords [0x5F,0x4D,0x24] {-_M$-}
-
-
-
--- FROM WORDS
-
-
-fromWords :: [Word8] -> Name
-fromWords words =
-  runST
-  (
-    do  mba <- newByteArray (List.length words)
-        writeWords mba 0 words
-        freeze mba
-  )
-
-
-writeWords :: MBA s -> Int -> [Word8] -> ST s ()
-writeWords !mba !i words =
-  case words of
-    [] ->
-      return ()
-
-    w:ws ->
-      do  writeWord8 mba i w
-          writeWords mba (i+1) ws
-
-
-
--- SEP BY
-
-
-sepBy :: Word8 -> Name -> Name -> Name
-sepBy (W8# sep#) (Utf8.Utf8 ba1#) (Utf8.Utf8 ba2#) =
-  let
-    !len1# = sizeofByteArray# ba1#
-    !len2# = sizeofByteArray# ba2#
-  in
-  runST
-  (
-    ST $ \s ->
-      case newByteArray# (len1# +# len2# +# 1#) s of
-        (# s, mba# #) ->
-          case copyByteArray# ba1# 0# mba# 0# len1# s of
-            s ->
-              case writeWord8Array# mba# len1# sep# s of
-                s ->
-                  case copyByteArray# ba2# 0# mba# (len1# +# 1#) len2# s of
-                    s ->
-                      case unsafeFreezeByteArray# mba# s of
-                        (# s, ba# #) -> (# s, Utf8.Utf8 ba# #)
-  )
-
-
-
--- PRIMITIVES
-
-
-data MBA s =
-  MBA# (MutableByteArray# s)
-
-
-{-# INLINE newByteArray #-}
-newByteArray :: Int -> ST s (MBA s)
-newByteArray (I# len#) =
-  ST $ \s ->
-    case newByteArray# len# s of
-      (# s, mba# #) -> (# s, MBA# mba# #)
-
-
-{-# INLINE freeze #-}
-freeze :: MBA s -> ST s Name
-freeze (MBA# mba#) =
-  ST $ \s ->
-    case unsafeFreezeByteArray# mba# s of
-      (# s, ba# #) -> (# s, Utf8.Utf8 ba# #)
-
-
-{-# INLINE writeWord8 #-}
-writeWord8 :: MBA s -> Int -> Word8 -> ST s ()
-writeWord8 (MBA# mba#) (I# offset#) (W8# w#) =
-  ST $ \s ->
-    case writeWord8Array# mba# offset# w# s of
-      s -> (# s, () #)
-
-
-{-# INLINE copyToMBA #-}
-copyToMBA :: Name -> MBA s -> ST s ()
-copyToMBA (Utf8.Utf8 ba#) (MBA# mba#) =
-    ST $ \s ->
-      case copyByteArray# ba# 0# mba# 0# (sizeofByteArray# ba#) s of
-        s -> (# s, () #)
-
-
-
--- COMMON NAMES
-
-
-{-# NOINLINE int #-}
-int :: Name
-int = fromChars "Int"
-
-
-{-# NOINLINE float #-}
-float :: Name
-float = fromChars "Float"
-
-
-{-# NOINLINE bool #-}
-bool :: Name
-bool = fromChars "Bool"
-
-
-{-# NOINLINE char #-}
-char :: Name
-char = fromChars "Char"
-
-
-{-# NOINLINE string #-}
-string :: Name
-string = fromChars "String"
-
-
-{-# NOINLINE maybe #-}
-maybe :: Name
-maybe = fromChars "Maybe"
-
-
-{-# NOINLINE result #-}
-result :: Name
-result = fromChars "Result"
-
-
-{-# NOINLINE list #-}
-list :: Name
-list = fromChars "List"
-
-
-{-# NOINLINE array #-}
-array :: Name
-array = fromChars "Array"
-
-
-{-# NOINLINE dict #-}
-dict :: Name
-dict = fromChars "Dict"
-
-
-{-# NOINLINE tuple #-}
-tuple :: Name
-tuple = fromChars "Tuple"
-
-
-{-# NOINLINE jsArray #-}
-jsArray :: Name
-jsArray = fromChars "JsArray"
-
-
-{-# NOINLINE task #-}
-task :: Name
-task = fromChars "Task"
-
-
-{-# NOINLINE router #-}
-router :: Name
-router = fromChars "Router"
-
-
-{-# NOINLINE cmd #-}
-cmd :: Name
-cmd = fromChars "Cmd"
-
-
-{-# NOINLINE sub #-}
-sub :: Name
-sub = fromChars "Sub"
-
-
-{-# NOINLINE platform #-}
-platform :: Name
-platform = fromChars "Platform"
-
-
-{-# NOINLINE virtualDom #-}
-virtualDom :: Name
-virtualDom = fromChars "VirtualDom"
-
-
-{-# NOINLINE shader #-}
-shader :: Name
-shader = fromChars "Shader"
-
-
-{-# NOINLINE debug #-}
-debug :: Name
-debug = fromChars "Debug"
-
-
-{-# NOINLINE debugger #-}
-debugger :: Name
-debugger = fromChars "Debugger"
-
-
-{-# NOINLINE bitwise #-}
-bitwise :: Name
-bitwise = fromChars "Bitwise"
-
-
-{-# NOINLINE basics #-}
-basics :: Name
-basics = fromChars "Basics"
-
-
-{-# NOINLINE utils #-}
-utils :: Name
-utils = fromChars "Utils"
-
-
-{-# NOINLINE negate #-}
-negate :: Name
-negate = fromChars "negate"
-
-
-{-# NOINLINE true #-}
-true :: Name
-true = fromChars "True"
-
-
-{-# NOINLINE false #-}
-false :: Name
-false = fromChars "False"
-
-
-{-# NOINLINE value #-}
-value :: Name
-value = fromChars "Value"
-
-
-{-# NOINLINE node #-}
-node :: Name
-node = fromChars "Node"
-
-
-{-# NOINLINE program #-}
-program :: Name
-program = fromChars "Program"
-
-
-{-# NOINLINE _main #-}
-_main :: Name
-_main = fromChars "main"
-
-
-{-# NOINLINE _Main #-}
-_Main :: Name
-_Main = fromChars "Main"
-
-
-{-# NOINLINE dollar #-}
-dollar :: Name
-dollar = fromChars "$"
-
-
-{-# NOINLINE identity #-}
-identity :: Name
-identity = fromChars "identity"
-
-
-{-# NOINLINE replModule #-}
-replModule :: Name
-replModule = fromChars "Canopy_Repl"
-
-
-{-# NOINLINE replValueToPrint #-}
-replValueToPrint :: Name
-replValueToPrint = fromChars "repl_input_value_"
+where
+
+-- Re-export all functionality from sub-modules
+
+import Data.Name.Constants
+import Data.Name.Core
+import Data.Name.Generation
+import Data.Name.Kernel
+import Data.Name.TypeVariable
+import Prelude hiding (maybe, negate)
--- compiler/src/Data/NonEmptyList.hs
+++ compiler/src/Data/NonEmptyList.hs
@@ -1,10 +1,91 @@
+-- |
+-- Module: Data.NonEmptyList
+-- Description: Non-empty list data structure for the Canopy compiler
+-- Copyright: (c) 2024 Canopy Contributors
+-- License: BSD-3-Clause
+--
+-- This module provides a non-empty list data structure that guarantees at
+-- compile time that the list contains at least one element. This prevents
+-- runtime errors from operations like 'head' and provides safer APIs for
+-- functions that require non-empty input.
+--
+-- The 'List' type is used throughout the Canopy compiler for collections
+-- that must contain at least one element, such as pattern match branches,
+-- function parameters, and error message collections.
+--
+-- ==== Key Features
+--
+-- * **Compile-time safety** - Prevents empty list errors at the type level
+-- * **Head-safe operations** - 'head' function cannot fail at runtime
+-- * **Standard instances** - Functor, Foldable, Traversable for compatibility
+-- * **Efficient operations** - Optimized sorting and append operations
+-- * **Binary serialization** - Built-in support for persistence and caching
+--
+-- ==== Usage Examples
+--
+-- @
+-- -- Create non-empty lists
+-- let single = singleton "error"
+--     multiple = List "first" ["second", "third"]
+--
+-- -- Safe head operation (never fails)
+-- let first_item = head multiple  -- "first"
+--
+-- -- Convert to regular list when needed
+-- let all_items = toList multiple  -- ["first", "second", "third"]
+--
+-- -- Append non-empty lists
+-- let combined = append single multiple
+--     -- Result: List "error" ["first", "second", "third"]
+--
+-- -- Sort by custom criteria
+-- let sorted = sortBy length (List "abc" ["a", "ab"])
+--     -- Result: List "a" ["ab", "abc"]
+-- @
+--
+-- ==== Architecture
+--
+-- Non-empty lists are represented using a single constructor 'List a [a]'
+-- that stores the head element separately from the tail. This ensures:
+--
+-- 1. **Type safety** - Construction requires at least one element
+-- 2. **Performance** - Head access is O(1) without pattern matching
+-- 3. **Memory efficiency** - Minimal overhead over regular lists
+-- 4. **Compatibility** - Easy conversion to/from standard lists
+--
+-- ==== Performance Characteristics
+--
+-- * **Construction**: O(1) for singleton, O(1) for List constructor
+-- * **Head access**: O(1) guaranteed safe access
+-- * **Append**: O(1) for prepending to head, O(n) for list append
+-- * **Sorting**: O(n log n) with specialized non-empty optimizations
+-- * **Conversion**: O(1) to list, O(1) from non-empty list
+--
+-- ==== Error Handling
+--
+-- This module eliminates the most common source of runtime errors in list
+-- processing by making non-emptiness a compile-time guarantee. Operations
+-- that would fail on empty lists (like 'head') are safe by construction.
+--
+-- @since 0.19.1
 module Data.NonEmptyList
-  ( List (..),
+  ( -- * Non-empty List Type
+    List (..),
+
+    -- * Construction
     singleton,
-    toList,
-    sortBy,
+
+    -- * Element Access
     head,
+
+    -- * Combination
     append,
+
+    -- * Transformation
+    sortBy,
+
+    -- * Conversion
+    toList,
   )
 where
 
@@ -13,61 +94,316 @@ import Data.Binary (Binary, get, put)
 import qualified Data.List as List
 import Prelude hiding (head)
 
--- LIST
-
+-- | Non-empty list data type.
+--
+-- A list that is guaranteed to contain at least one element, represented
+-- by storing the head element separately from the (possibly empty) tail.
+-- This provides compile-time safety for operations that require non-empty lists.
+--
+-- ==== Constructor
+--
+-- * 'List' @head tail@ - Creates a non-empty list with @head@ as the first
+--   element and @tail@ as the remaining elements
+--
+-- ==== Examples
+--
+-- >>> List 1 []
+-- List 1 []
+--
+-- >>> List 'a' ['b', 'c']
+-- List 'a' ['b','c']
+--
+-- >>> toList (List "first" ["second", "third"])
+-- ["first", "second", "third"]
+--
+-- @since 0.19.1
 data List a
-  = List a [a]
+  = -- | Non-empty list with head and tail
+    List a [a]
 
+-- | Create a singleton non-empty list containing one element.
+--
+-- This is the most concise way to create a non-empty list with exactly
+-- one element, equivalent to @List element []@.
+--
+-- ==== Examples
+--
+-- >>> singleton 42
+-- List 42 []
+--
+-- >>> singleton "hello"
+-- List "hello" []
+--
+-- >>> toList (singleton 'x')
+-- ['x']
+--
+-- ==== Use Cases
+--
+-- * Creating single-element collections that must be non-empty
+-- * Base case for building larger non-empty lists
+-- * Converting single values to non-empty list context
+--
+-- @since 0.19.1
 singleton :: a -> List a
 singleton a =
   List a []
 
+-- | Convert a non-empty list to a regular list.
+--
+-- This function provides safe conversion from the type-safe non-empty
+-- list to a regular list for interoperability with standard list operations.
+-- The resulting list is guaranteed to be non-empty.
+--
+-- ==== Examples
+--
+-- >>> toList (singleton 1)
+-- [1]
+--
+-- >>> toList (List 'a' ['b', 'c'])
+-- ['a', 'b', 'c']
+--
+-- >>> length (toList (List "x" ["y", "z"]))
+-- 3
+--
+-- ==== Properties
+--
+-- * **Non-empty guarantee**: Result is always non-empty
+-- * **Order preservation**: Elements maintain their original order
+-- * **No-copy conversion**: O(1) operation using cons
+--
+-- @since 0.19.1
 toList :: List a -> [a]
 toList (List x xs) =
   x : xs
 
+-- | Get the first element of a non-empty list.
+--
+-- This function provides safe access to the head element without any
+-- possibility of runtime failure, unlike the standard 'head' function
+-- on regular lists which can fail on empty lists.
+--
+-- ==== Examples
+--
+-- >>> head (singleton "test")
+-- "test"
+--
+-- >>> head (List 1 [2, 3, 4])
+-- 1
+--
+-- >>> head (List 'a' [])
+-- 'a'
+--
+-- ==== Performance
+--
+-- * **Time Complexity**: O(1)
+-- * **Space Complexity**: O(1)
+-- * **Safety**: Cannot fail at runtime
+--
+-- @since 0.19.1
 head :: List a -> a
 head (List x _) = x
 
+-- | Append two non-empty lists to create a new non-empty list.
+--
+-- This function combines two non-empty lists by using the head of the first
+-- list and appending its tail to the complete second list. The result is
+-- guaranteed to be non-empty since both inputs are non-empty.
+--
+-- ==== Examples
+--
+-- >>> append (singleton 1) (singleton 2)
+-- List 1 [2]
+--
+-- >>> append (List 'a' ['b']) (List 'c' ['d'])
+-- List 'a' ['b', 'c', 'd']
+--
+-- >>> toList (append (List 1 [2]) (List 3 [4, 5]))
+-- [1, 2, 3, 4, 5]
+--
+-- ==== Properties
+--
+-- * **Non-empty result**: Always produces a non-empty list
+-- * **Order preservation**: Elements from first list come before elements from second
+-- * **Head preservation**: Head of result is head of first list
+--
+-- ==== Performance
+--
+-- * **Time Complexity**: O(n) where n is the length of the first list's tail
+-- * **Space Complexity**: O(n) for the combined tail
+-- * **Memory**: Single allocation for the new tail list
+--
+-- @since 0.19.1
 append :: List a -> List a -> List a
 append (List x xs) ys = List x (xs <> toList ys)
 
--- INSTANCES
-
+-- | Functor instance for non-empty lists.
+--
+-- Applies a function to every element in the non-empty list while
+-- preserving the non-empty structure and element order.
+--
+-- ==== Laws
+--
+-- * @fmap id == id@
+-- * @fmap (f . g) == fmap f . fmap g@
+--
+-- ==== Examples
+--
+-- >>> fmap (*2) (List 1 [2, 3])
+-- List 2 [4, 6]
+--
+-- >>> fmap show (singleton 42)
+-- List "42" []
+--
+-- @since 0.19.1
 instance Functor List where
   fmap func (List x xs) = List (func x) (fmap func xs)
 
+-- | Traversable instance for non-empty lists.
+--
+-- Enables effectful operations over non-empty lists while preserving
+-- the non-empty structure. Useful for operations that may fail or
+-- have side effects.
+--
+-- ==== Examples
+--
+-- >>> traverse Just (List 1 [2, 3])
+-- Just (List 1 [2, 3])
+--
+-- >>> traverse (\\x -> if x > 0 then Just x else Nothing) (List 1 [2])
+-- Just (List 1 [2])
+--
+-- @since 0.19.1
 instance Traversable List where
   traverse func (List x xs) = List <$> func x <*> traverse func xs
 
+-- | Foldable instance for non-empty lists.
+--
+-- Provides fold operations that work over the entire non-empty list.
+-- The specialized 'foldl1' implementation is safe since the list is
+-- guaranteed to be non-empty.
+--
+-- ==== Examples
+--
+-- >>> foldr (+) 0 (List 1 [2, 3, 4])
+-- 10
+--
+-- >>> foldl1 max (List 3 [1, 4, 2])
+-- 4
+--
+-- @since 0.19.1
 instance Foldable List where
   foldr step state (List x xs) = step x (foldr step state xs)
   foldl step state (List x xs) = foldl step (step state x) xs
   foldl1 step (List x xs) = foldl step x xs
 
+-- | Equality instance for non-empty lists.
+--
+-- Two non-empty lists are equal if their heads are equal and
+-- their tails are equal (element-wise comparison).
+--
+-- ==== Examples
+--
+-- >>> List 1 [2, 3] == List 1 [2, 3]
+-- True
+--
+-- >>> singleton 'a' == singleton 'b'
+-- False
+--
+-- @since 0.19.1
 instance (Eq a) => Eq (List a) where
   List x xs == List y ys = x == y && xs == ys
 
+-- | Show instance for non-empty lists.
+--
+-- Displays non-empty lists by converting them to regular lists
+-- and using the standard list show format.
+--
+-- ==== Examples
+--
+-- >>> show (List 1 [2, 3])
+-- "[1,2,3]"
+--
+-- >>> show (singleton "hello")
+-- "[\"hello\"]"
+--
+-- @since 0.19.1
 instance (Show a) => Show (List a) where
   show xs = show (toList xs)
 
--- SORT BY
-
+-- | Sort a non-empty list by a ranking function.
+--
+-- This function sorts the non-empty list using the provided ranking function
+-- while maintaining the non-empty property. The implementation optimizes for
+-- the case where the head element is already in the correct position.
+--
+-- ==== Examples
+--
+-- >>> sortBy length (List "abc" ["a", "ab"])
+-- List "a" ["ab", "abc"]
+--
+-- >>> sortBy id (List 3 [1, 4, 2])
+-- List 1 [2, 3, 4]
+--
+-- >>> sortBy Down (singleton 5)
+-- List 5 []
+--
+-- ==== Algorithm
+--
+-- The implementation:
+-- 1. Sorts the tail using standard list sorting
+-- 2. Determines the correct position for the head element
+-- 3. Constructs the result with optimal head positioning
+--
+-- ==== Performance
+--
+-- * **Time Complexity**: O(n log n) where n is the total number of elements
+-- * **Space Complexity**: O(n) for the sorted result
+-- * **Memory**: Single allocation with optimized head placement
+--
+-- ==== Properties
+--
+-- * **Non-empty result**: Always produces a non-empty list
+-- * **Stable sorting**: Elements with equal ranking maintain relative order
+-- * **Total ordering**: Works with any 'Ord' instance
+--
+-- @since 0.19.1
 sortBy :: (Ord b) => (a -> b) -> List a -> List a
 sortBy toRank (List x xs) =
-  let comparison a b =
-        compare (toRank a) (toRank b)
-   in case List.sortBy comparison xs of
-        [] ->
-          List x []
-        y : ys ->
-          case comparison x y of
-            LT -> List x (y : ys)
-            EQ -> List x (y : ys)
-            GT -> List y (List.insertBy comparison x ys)
+  case List.sortBy comparison xs of
+    [] ->
+      List x []
+    y : ys ->
+      case comparison x y of
+        LT -> List x (y : ys)
+        EQ -> List x (y : ys)
+        GT -> List y (List.insertBy comparison x ys)
+  where
+    comparison a b = compare (toRank a) (toRank b)
 
--- BINARY
-
+-- | Binary serialization instance for non-empty lists.
+--
+-- Provides efficient serialization and deserialization of non-empty
+-- lists for compiler caching, intermediate file storage, and network
+-- communication. The serialization preserves both the head element
+-- and the tail structure.
+--
+-- ==== Examples
+--
+-- >>> let encoded = encode (List 1 [2, 3])
+-- >>> decode encoded :: List Int
+-- List 1 [2, 3]
+--
+-- >>> encode (singleton "test") == encode (List "test" [])
+-- True
+--
+-- ==== Use Cases
+--
+-- * Compiler intermediate file storage with guaranteed non-empty collections
+-- * Caching parsed structures that must contain elements
+-- * Network protocols requiring non-empty data
+-- * Persistent storage of non-empty configurations
+--
+-- @since 0.19.1
 instance (Binary a) => Binary (List a) where
   put (List x xs) = put x >> put xs
   get = liftM2 List get get
